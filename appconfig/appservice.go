2a55196e462ef093d14da8f463bc34d3672b0f43
Prateek Gupta <pgupta@deqode.com>
2021-04-12 12:47:10 +0530

    Update: appservice for versionId

--- appconfig/appservice.go
+++ appconfig/appservice.go
@@ -2,43 +2,44 @@ package appconfig
 
 import (
 	"errors"
+
 	"github.com/deqodelabs/IaaC/appconfig/pb"
 	"github.com/deqodelabs/IaaC/appconfig/tools"
 	"github.com/deqodelabs/IaaC/appconfig/validate"
-	"github.com/nu7hatch/gouuid"
+	uuid "github.com/nu7hatch/gouuid"
 	"github.com/philippgille/gokv"
 	"go.uber.org/zap"
 )
 
-type AppService	struct{
-	Store gokv.Store
-	Logger zap.Logger
+type AppService struct {
+	Store  gokv.Store
+	Logger *zap.Logger
 }
 
-type VersionRange struct{
-	LastVersion int32
+type VersionRange struct {
+	LastVersion   int32
 	LatestVersion int32
 }
 
-func(s *AppService) ValidateAppConfig(app *pb.AppConfig) error {
+func (s *AppService) ValidateAppConfig(app *pb.AppConfig) error {
 	s.Logger.Debug("validate app config method called with:", zap.Any("app", app))
 	err := app.Validate()
-	if err != nil{
+	if err != nil {
 		s.Logger.Error("error:", zap.Error(err))
 		return err
 	}
 	err = validate.CustomValidate(app)
-	if err != nil{
+	if err != nil {
 		s.Logger.Error("error:", zap.Error(err))
 		return err
 	}
 	return nil
 }
 
-func (s *AppService) Save(app *pb.AppConfig) (*pb.AppConfig, error){
+func (s *AppService) Save(app *pb.AppConfig) (*pb.AppConfig, error) {
 	s.Logger.Debug("save app config method called with:", zap.Any("app", app))
 	err := s.ValidateAppConfig(app)
-	if err != nil{
+	if err != nil {
 		s.Logger.Error("error:", zap.Error(err))
 		return app, err
 	}
@@ -47,10 +48,10 @@ func (s *AppService) Save(app *pb.AppConfig) (*pb.AppConfig, error){
 	if app.AppId == "" {
 		id, _ := uuid.NewV4()
 		app.AppId = id.String()
-		versionRange = VersionRange{1,1}
+		versionRange = VersionRange{1, 1}
 		versionId = tools.GenerateVersionId(app.AppId, 1)
-	}else{
-		found, err := s.Store.Get(app.AppId, versionRange)
+	} else {
+		found, err := s.Store.Get(app.AppId, &versionRange)
 		if err != nil {
 			s.Logger.Error("error:", zap.Error(err))
 			return app, err
@@ -64,10 +65,10 @@ func (s *AppService) Save(app *pb.AppConfig) (*pb.AppConfig, error){
 		if app.Version < latestVersion {
 			s.Logger.Error("error: version already exist")
 			return app, errors.New("version already exist")
-		}else if app.Version > latestVersion {
+		} else if app.Version > latestVersion {
 			s.Logger.Error("error: version missed")
 			return app, errors.New("version missed")
-		}else{
+		} else {
 			versionRange.LatestVersion++
 		}
 		versionId = tools.GenerateVersionId(app.AppId, latestVersion)
@@ -85,11 +86,11 @@ func (s *AppService) Save(app *pb.AppConfig) (*pb.AppConfig, error){
 	return app, nil
 }
 
-func (s *AppService) GetAppConfig(id string) (*pb.AppConfig, error){
+func (s *AppService) GetAppConfig(id string) (*pb.AppConfig, error) {
 	s.Logger.Debug("get app config method called with:", zap.String("id", id))
 	var versionRange VersionRange
-	var appConfig *pb.AppConfig
-	found, err := s.Store.Get(id, versionRange)
+	var appConfig pb.AppConfig
+	found, err := s.Store.Get(id, &versionRange)
 	if err != nil {
 		s.Logger.Error("error:", zap.Error(err))
 		return nil, err
@@ -99,7 +100,7 @@ func (s *AppService) GetAppConfig(id string) (*pb.AppConfig, error){
 		return nil, errors.New("id for app config not found")
 	}
 	versionId := tools.GenerateVersionId(id, versionRange.LatestVersion)
-	found, err = s.Store.Get(versionId, appConfig)
+	found, err = s.Store.Get(versionId, &appConfig)
 	if err != nil {
 		s.Logger.Error("error:", zap.Error(err))
 		return nil, err
@@ -108,10 +109,10 @@ func (s *AppService) GetAppConfig(id string) (*pb.AppConfig, error){
 		s.Logger.Error("error: app config for version not found")
 		panic("app config for version not found")
 	}
-	return appConfig, nil
+	return &appConfig, nil
 }
 
-func (s *AppService) GetAppConfigForVersion(id string, version int32) (*pb.AppConfig, error){
+func (s *AppService) GetAppConfigForVersion(id string, version int32) (*pb.AppConfig, error) {
 	s.Logger.Debug("get app config for version method called with:", zap.String("id", id), zap.Int32("version", version))
 	var versionRange VersionRange
 	var appConfig *pb.AppConfig
@@ -127,7 +128,7 @@ func (s *AppService) GetAppConfigForVersion(id string, version int32) (*pb.AppCo
 	if version < versionRange.LastVersion {
 		s.Logger.Error("error: version is deprecated")
 		return nil, errors.New("version is deprecated")
-	}else if version > versionRange.LatestVersion {
+	} else if version > versionRange.LatestVersion {
 		s.Logger.Error("error: version does not exist")
 		return nil, errors.New("version does not exist")
 	}
@@ -143,4 +144,3 @@ func (s *AppService) GetAppConfigForVersion(id string, version int32) (*pb.AppCo
 	}
 	return appConfig, nil
 }
-
